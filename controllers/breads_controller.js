a646c362af2b943a9fee9fb9a028134be3c8106b
DJFancyAl <abluemleiv@gmail.com>
2023-03-08 22:11:36 -0500

    Completed the exercise

---
+++ controllers/breads_controller.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const Bread = require('../models/breads')
+const breads = express.Router()
+
+// INDEX
+breads.get('/', (req, res) => {
+  res.send(Bread)
+})
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+  })
+  
+
+module.exports = breads
---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT
+
+console.log(PORT)
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads!')
+})
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('Listening on port:', PORT);
+})  
