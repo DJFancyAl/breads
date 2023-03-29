c618172924243c507d63063eaa06e23f58dd83bc
DJFancyAl <abluemleiv@gmail.com>
2023-03-29 19:18:53 -0400

    Improved styling on show and index pages.

--- views/show.jsx
+++ views/show.jsx
@@ -4,23 +4,26 @@ const Default = require('./layouts/Default')
 function Show ( {bread, index} ) {
     return (
       <Default title={bread.name + " Details"}>
-        <h2>{bread.name} Bread</h2>
-        <h5>{bread.name} bread 
-        {bread.hasGluten ?
-        <span> has gluten.</span> :
-        <span> does not have gluten.</span>}
-        </h5>
-        
-        <img src={bread.image} alt={bread.name} />
+        <main>
+          <h2>{bread.name} Bread</h2>
+          <h5>{bread.name} bread 
+          {bread.hasGluten ?
+          <span> has gluten.</span> :
+          <span> does not have gluten.</span>}
+          </h5>
+          
+          <img src={bread.image} alt={bread.name} />
 
-        <div className='button-group'>
-          <a href={`/breads/${index}/edit`}><button className='editButton'>Edit</button></a>
+          <div className='button-group'>
+            <a href={`/breads/${index}/edit`}><button className='editButton'>Edit</button></a>
 
-          <form className='deleteButton' action={`/breads/${index}?_method=DELETE`} method="POST">
-            <input type='submit' value="DELETE" className='deleteButton' />
-          </form>
-        </div>
-
+            <form className='deleteButton' action={`/breads/${index}?_method=DELETE`} method="POST">
+              <input type='submit' value="DELETE" className='deleteButton' />
+            </form>
+            
+          </div>
+          <a href="/breads"><button className='newButton'>Return to Bread List</button></a>
+        </main>
       </Default>
     )
 }
