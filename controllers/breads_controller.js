f6f081544a50cfbd0f52b3a49f1b061aedde9960
DJFancyAl <abluemleiv@gmail.com>
2023-03-31 12:27:08 -0400

    Completed Activity and Bonus

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -33,6 +33,35 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
+// Create Many
+breads.get('/data/seed', (req, res) => {
+  Bread.insertMany([
+      {
+        name: 'Rye',
+        hasGluten: true,
+        image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+      },
+      {
+        name: 'French',
+        hasGluten: true,
+        image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+      },
+      {
+        name: 'Gluten Free',
+        hasGluten: false,
+        image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+      },
+      {
+        name: 'Pumpernickel',
+        hasGluten: true,
+        image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+      }
+    ])
+    .then(createdBreads => {
+      res.redirect('/breads')
+    })
+})
+
 
 // SHOW
 breads.get('/:id', (req, res) => {
@@ -50,9 +79,10 @@ breads.get('/:id', (req, res) => {
 
 // DELETE
 breads.delete('/:id', (req, res) => {
-  Bread.findByIdAndDelete(req.params.id).then(
+  Bread.findByIdAndDelete(req.params.id)
+  .then(deletedBread => {
     res.status(303).redirect('/breads')
-  )
+  })
   .catch(err => {
     res.status(404).render('404')
   })
@@ -65,9 +95,10 @@ breads.put('/:id', (req, res) => {
   } else {
     req.body.hasGluten = false
   }
-  Bread.findByIdAndUpdate(req.params.id, req.body).then(
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  .then(updatedBread => {
     res.redirect(`/breads/${req.params.id}`)
-  )
+  })
   .catch(err => {
     res.status(404).render('404')
   })
--- views/show.jsx
+++ views/show.jsx
@@ -2,29 +2,31 @@ const React = require('react')
 const Default = require('./layouts/Default')
 
 function Show ( {bread} ) {
-    return (
-      <Default title={bread.name + " Details"}>
-        <main>
-          <h2>{bread.name} Bread</h2>
-          <h5>{bread.name} bread 
-          {bread.hasGluten ?
-          <span> has gluten.</span> :
-          <span> does not have gluten.</span>}
-          </h5>
+
+  return (
+    <Default title={bread.name + " Details"}>
+      <main>
+        <h2>{bread.name} Bread</h2>
+        <h5>{bread.name} bread 
+        {bread.hasGluten ?
+        <span> has gluten.</span> :
+        <span> does not have gluten.</span>}
+        </h5>
+        
+        <img src={bread.image} alt={bread.name} onClick={() => { console.log("button clicked");}} />
+
+        <div className='button-group'>
+          <a href={`/breads/${bread.id}/edit`}><button className='editButton'>Edit</button></a>
+
+          <form className='deleteButton' action={`/breads/${bread.id}?_method=DELETE`} method="POST">
+            <input type='submit' value="DELETE" className='deleteButton' />
+          </form>
           
-          <img src={bread.image} alt={bread.name} />
-
-          <div className='button-group'>
-            <a href={`/breads/${bread.id}/edit`}><button className='editButton'>Edit</button></a>
-
-            <form className='deleteButton' action={`/breads/${bread.id}?_method=DELETE`} method="POST">
-              <input type='submit' value="DELETE" className='deleteButton' />
-            </form>
-            
-          </div>
-          <a href="/breads"><button className='newButton'>Return to Bread List</button></a>
-        </main>
-      </Default>
+        </div>
+        <a href="/breads"><button className='newButton'>Return to Bread List</button></a>
+      </main>
+      <audio id='sound' src='/Eating.mp3' />
+    </Default>
     )
 }
 
