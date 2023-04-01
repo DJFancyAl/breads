11b9c75729f444bc21ba7a98371a759766de00fc
DJFancyAl <abluemleiv@gmail.com>
2023-04-01 10:33:19 -0400

    Added Seeds to a Different File

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Bread = require('../models/breads')
+const seeds = require('../models/seeds')
 const breads = express.Router()
 
 // INDEX
@@ -29,34 +30,14 @@ breads.post('/', (req, res) => {
   } else {
     req.body.hasGluten = false
   }
-  Bread.create(req.body)
-  res.redirect('/breads')
+  Bread.create(req.body).then(() => {
+    res.redirect('/breads')
+  })
 })
 
 // Create Many
 breads.get('/data/seed', (req, res) => {
-  Bread.insertMany([
-      {
-        name: 'Rye',
-        hasGluten: true,
-        image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-      },
-      {
-        name: 'French',
-        hasGluten: true,
-        image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-      },
-      {
-        name: 'Gluten Free',
-        hasGluten: false,
-        image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-      },
-      {
-        name: 'Pumpernickel',
-        hasGluten: true,
-        image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-      }
-    ])
+  Bread.insertMany(seeds)
     .then(createdBreads => {
       res.redirect('/breads')
     })
